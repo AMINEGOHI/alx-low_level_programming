149b64d8ad09b03f3fa36b49302a459c0235b203
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:28:05 -0700

    4 alphabet lower upper no e q task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - this is main function
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char alpha;
+	/* your code goes there */
+	for (alpha = 'a'; alpha <= 'z'; alpha++){
+		if( alpha =='q' || alpha == 'e')
+			continue;
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
