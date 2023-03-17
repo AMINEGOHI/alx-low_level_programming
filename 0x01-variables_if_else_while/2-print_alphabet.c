dc586fb7147a35ecac12c1c0d5e4c0b7d903b87d
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:00:06 -0700

    2 alphabet task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-        char alphabet;
-        for (alphabet='a'; alphabet< 'z'; alphabet++)
-        {
-                putchar(alphabet);
+	char alphabet ;
+        for (alphabet='a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
         }
-
         return (0);
 }
