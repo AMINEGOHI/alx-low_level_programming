7960c72ca401193f2c734ee5e464ce340efd4260
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:05:23 -0700

    2 alphabet task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,19 @@
 #include <time.h>
 /* more headers goes there */
 /**
- * main - this is main function desription
+ * main - this is main function
  * Return: Always 0
  */
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char alphabet ;
-        for (alphabet='a'; alphabet <= 'z'; alphabet++)
-	{
-		putchar(alphabet);
-        }
-        return (0);
+	char alpha;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	for( alpha = 'a'; alpha <= 'z'; alpha++ ){
+		putchar(alpha);
+	}
+	return (0);
 }
