941724b4b44a5f0679a4256c36eafdaa8fadcada
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:11:40 -0700

    2 alphabet task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char alpha;
 	/* your code goes there */
-	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		putchar(alpha);
 	putchar('\n');
 	return (0);
