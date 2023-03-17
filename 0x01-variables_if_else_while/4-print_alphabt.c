537bca98e05d2b4bf4c41aa3c1f8f768f47c37e6
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:29:48 -0700

    4 alphabet lower upper no e q task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,9 @@ int main(void)
 {
 	char alpha;
 	/* your code goes there */
-	for (alpha = 'a'; alpha <= 'z'; alpha++){
-		if( alpha =='q' || alpha == 'e')
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if(alpha == 'q' || alpha == 'e')
 			continue;
 		putchar(alpha);
 	}
