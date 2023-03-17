22ec423f74e624361f6fe735d7a8dbae8e20b41a
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 12:05:05 -0700

    8 hexanumeric  task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - this is main function
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+	/* your code goes there */
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10) 
+			putchar('0' + i);
+		else
+			putchar('a' + i - 10);
+        }
+	putchar('\n');
+	return (0);
+}
