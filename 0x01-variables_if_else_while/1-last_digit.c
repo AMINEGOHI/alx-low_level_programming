41355835f13e0e56e30844de348c3e81d3d2923e
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 04:26:10 -0700

    1. The last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int l = n % 10;
-	if (l > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-	else if (l < 6 && l!=0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	int lastDegit= n % 10;
+	if (lastDegit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDegit);
+	else if (lastDegit < 6 && lastDegit!=0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDegit);
 	else
-		printf("Last digit of %d is %d and is 0\n", n, l);
+		printf("Last digit of %d is %d and is 0\n", n, lastDegit);
 	return (0);
 }
