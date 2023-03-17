a6dedd445fa0cb5229c9c26544c76db0efc0731a
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:21:07 -0700

    3 alphabet lower upper task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - this is main function
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char alpha;
+	/* your code goes there */
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+                putchar(alpha);
+	putchar('\n');
+	return (0);
+}
