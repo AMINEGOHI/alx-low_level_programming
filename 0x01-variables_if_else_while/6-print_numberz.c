af94036c4302c833123fa81f01e35b4966ce06ae
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-17 11:41:22 -0700

    6 putchr numeric  task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - this is main function
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+	/* your code goes there */
+	for (i = 0; i <= 9; i++)
+		putchar(char(i));
+	putchar('\n');
+	return (0);
+}
