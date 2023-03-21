caee60a74597d27dd7d3228a2d1de211ebf41cd7
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-21 05:36:22 -0700

    2 task aphabet lowecase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,11 @@
  * print_alphabet - prints the alphabet
  * Return: void
  */
-void print_alphabet(void){
-	char alphabet='a';
-	while(alphabet<='z')
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
 		_putchar(alphabet++);
 	_putchar('\n');
 }
