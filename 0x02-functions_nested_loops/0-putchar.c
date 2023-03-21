2c9e5ce902484ce8c83c6f7878d6dd530e24043d
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-21 05:18:58 -0700

    1 task putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - a simple program that outputs "_putchar"
  * Return: 0 on success
  */
 int main()
 {
-    printf("_putchar");
+	char str[] = "_putchar\n";
+	int i = 0;
 
-    return 0;
+	while (str[i] != '\0')
+		_putchar(str[i++]);
+	return (0);
+
+  
 }
