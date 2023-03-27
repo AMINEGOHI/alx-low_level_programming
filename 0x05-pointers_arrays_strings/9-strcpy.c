36c85fdc30a36027da7fb6b6da83aa6e9daf008e
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-27 16:02:20 -0700

    9task fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,14 @@
  * @src: source string
  *
  * Return: pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
+ * */
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	return (dest);
+char *_strcpy(char *dest, char *src) {
+    char *p = dest;
+    while (*src != '\0') {
+        *p++ = *src++;
+    }
+    *p = '\0'; // append null terminator
+    return dest;
 }
+
