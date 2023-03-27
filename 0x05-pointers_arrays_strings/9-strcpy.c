4bc6da2370138b93aae05565a5f538688fc74986
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-03-27 16:06:48 -0700

    9task fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
+
 /**
- * _strcpy - copies a string from one buffer to another
+ * char *_strcpy - Copy a string to a new space in memory
+ * @dest: destination to copy to
+ * @src: source string to copy
  *
- * @dest: destination buffer
- * @src: source string
- *
- * Return: pointer to dest
- * */
+ * Return: Pointer to dest
+ */
 
 char *_strcpy(char *dest, char *src)
 {
 	char *p = dest;
 	while (*src != '\0')
-       	{
+	{
 		*p++ = *src++;
 	}
 	*p = '\0';
