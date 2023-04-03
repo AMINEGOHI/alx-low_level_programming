687beee7245b36126e8c9f7cf8fed212cf01a195
Amine KHARBOUCH <kharbouch.amine@yahoo.fr>
2023-04-03 08:28:27 -0700

    commit task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * memcpy - prints buffer in hexa
+ * @dest: the address memory to print in
+ * @src:  the assress memory to read from
+ * @n: the size of the memory to print
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
